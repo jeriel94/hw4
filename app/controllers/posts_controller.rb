fd2099c281d7ff96be50e5d8266425976bcc0e7e
Jeriel Acosta <jeriel.acosta@kellogg.northwestern.edu>
2023-06-02 01:26:13 +0000

    controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,29 @@
 class PostsController < ApplicationController
 
   def new
-    @post = Post.new
-    @post.place_id = params["place_id"]
+    if @current_user
+      @post = Post.new
+      @post.place_id = params["place_id"]
+      @post.user_id = @current_user["id"]
+    else
+      flash["notice"] = "Login first."
+    end
   end
 
   def create
-    @post = Post.new
-    @post["title"] = params["post"]["title"]
-    @post["description"] = params["post"]["description"]
-    @post["posted_on"] = params["post"]["posted_on"]
-    @post["place_id"] = params["post"]["place_id"]
-    @post.save
-    redirect_to "/places/#{@post["place_id"]}"
+    if @current_user
+      @post = Post.new
+      @post["title"] = params["post"]["title"]
+      @post["description"] = params["post"]["description"]
+      @post["posted_on"] = params["post"]["posted_on"]
+      @post["place_id"] = params["post"]["place_id"]
+      @post.uploaded_image.attach(params["post"]["uploaded_image"])
+      @post["user_id"] = @current_user["id"]
+      @post.save
+      redirect_to "/places/#{@post["place_id"]}"
+    else
+      flash["notice"] = "Login first."
+    end
   end
 
 end
